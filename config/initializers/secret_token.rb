b5873eeee8acfe1f59a92ef4e57d6ea4fecd494c
plasma <plasma@Blue.(none)>
2014-01-30 13:01:26 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Test0::Application.config.secret_key_base = '6cdded750e48ff55f4cc3edec8957892d43198cc8e428eedad557bddcc462ab637370bac8a7e61743157e583b5f7fbcdde777b947c43f0fc025738f793c79f41'
